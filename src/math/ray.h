f02ffb3cd67a79792427e28565df22b3300ae0ed
Peter Szabo <peter.szabo13@gmail.com>
2024-08-16 21:55:21 +0200

    Ray class

---
+++ src/math/ray.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "vec3.h"
+
+class ray {
+  public:
+    ray() {}
+
+    ray(const point3& origin, const vec3& direction) : o(origin), dir(direction) {}
+
+    const point3& origin() const  { return o; }
+    const vec3& direction() const { return dir; }
+
+    point3 at(double t) const {
+        return o + t*dir;
+    }
+
+  private:
+    point3 o;
+    vec3 dir;
+};
