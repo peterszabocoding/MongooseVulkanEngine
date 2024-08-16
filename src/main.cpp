e56fd30abeed9245e59078536aff415a774c1e15
Peter Szabo <peter.szabo13@gmail.com>
2024-08-16 21:50:43 +0200

    Added vec3 and color classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
+#include "math/color.h"
+#include "math/vec3.h"
 
 int main() {
 
     // Image
 
-    int image_width = 256;
-    int image_height = 256;
+    int image_width = 800;
+    int image_height = 800;
 
     // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixelColor(
+                double(i) / (image_width-1), 
+                double(j) / (image_height-1), 
+                0.0);
+            write_color(std::cout, pixelColor);
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
